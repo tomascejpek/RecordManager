361d628ac81586f18eeda47c0412cbe3c14bd7fa
Michal Merta <merta.m@gmail.com>
2013-11-15 10:44:11 +0100

    added record classes for VNF

---
+++ classes/VnfMarc/VnfMarcRecord.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * MarcRecord Class
+ *
+ * PHP version 5
+ *
+ * Copyright (C) The National Library of Finland 2011-2013
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category DataManagement
+ * @package  RecordManager
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://github.com/KDK-Alli/RecordManager
+ */
+
+require_once __DIR__.'/../MarcRecord.php';
+require_once __DIR__.'/../MetadataUtils.php';
+require_once __DIR__.'/../Logger.php';
+
+/**
+ * MarcRecord Class - local customization for VNF
+ *
+ * This is a class for processing MARC records.
+ *
+ * @category DataManagement
+ * @package  RecordManager
+ * @author   Michal Merta <merta.m@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://github.com/moravianlibrary/RecordManager
+ */
+class VnfMarcRecord extends MarcRecord
+{
+    public function toSolrArray() {
+        
+        $data = parent::toSolrArray();
+        
+        foreach (parent::getUniqueIDs() as $uid) {
+            if (strlen($uid) > 5) {
+                $prefix = substr($uid, 1,3);
+                if ($prefix == 'ian') {
+                    $data['ean_str_mv'] = substr($uid,5);
+                }
+            }
+        }
+        
+        return $data;
+    }
+
+}
