2d4f2d5d9c7f272a212869f1e93203c1714fc1a5
Michal Merta <merta.m@gmail.com>
2013-12-05 09:24:39 +0100

    added record classes for Historicke fondy

---
+++ classes/HistMarc/HistoricalMarcRecord.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * MarcRecord Class
+ *
+ * PHP version 5
+ *
+ * Copyright (C) The National Library of Finland 2011-2013
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category DataManagement
+ * @package  RecordManager
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://github.com/KDK-Alli/RecordManager
+ */
+
+require_once __DIR__.'/../MarcRecord.php';
+require_once __DIR__.'/../MetadataUtils.php';
+require_once __DIR__.'/../Logger.php';
+
+/**
+ * MarcRecord Class - local customization for MZK
+ *
+ * This is a class for processing MARC records.
+ *
+ * @category DataManagement
+ * @package  RecordManager
+ * @author   Michal Merta
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://github.com/moravianlibrary/RecordManager
+ */
+class HistoricalMarcRecord extends MarcRecord
+{
+
+    /**
+     * Constructor
+     *
+     * @param string $data   Metadata
+     * @param string $oaiID  Record ID received from OAI-PMH (or empty string for file import)
+     * @param string $source Source ID
+     */
+    public function __construct($data, $oaiID, $source)
+    {
+        parent::__construct($data, $oaiID, $source);
+    }
+    
+    public function toSolrArray() {
+        $data = parent::toSolrArray();
+        
+        if (isset($data['url']) && is_array($data['url'])
+                && isset($data['url'][0])) {
+            $data['status'] = 'online';
+        } else {
+            $data['status'] = 'offline';
+        }
+        
+        $field = parent::getField('260');
+        if ($field) {
+            $year = parent::getSubfield($field, 'c');
+            $matches = array();
+            if ($year && preg_match('/(\d{4})/', $year, $matches)) {
+                $data['publishDate_display'] = $matches[1];
+            }
+        }
+    
+        return $data;
+    }  
+} 
+
