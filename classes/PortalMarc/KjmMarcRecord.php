0aedefd9414a442140a0fa36b7016d45e3e188c9
Michal Merta <merta.m@gmail.com>
2014-06-10 09:53:23 +0200

    fixed kjm harvesting

--- classes/PortalMarc/KjmMarcRecord.php
+++ classes/PortalMarc/KjmMarcRecord.php
@@ -26,11 +26,48 @@ class KjmMarcRecord extends PortalMarcRecord
         parent::__construct($data, $oaiID, $source);
     }
 
-    public function toSolrArray() {
+    public function toSolrArray() 
+    {
         $data = parent::toSolrArray();
         
         $data['institution'] = $this->getHierarchicalInstitutions('993', 'l');
         return $data;
+    }
+
+    protected function parseXML($xml)
+    {
+       $document = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOENT);
+       if ($document === false) {
+             throw new Exception('MarcRecord: failed to parse from XML');
+       }
+
+       $query = $document->leader;
+       if ($query) {
+           $this->fields['000'] = $query->__toString();
+       } else {
+           $this->fields['000'] = '';
+       }
+
+       $query = $document->controlfield;
+       foreach ($query as $field) {
+           $this->fields[$field->attributes()->tag->__toString()] = $field->__toString();
+       }
+
+       $query = $document->datafield;
+       foreach ($query as $field) {
+           $newField = array(
+              'i1' => str_pad($field->attributes()->ind1 ? $field->attributes()->ind1->__toString() : '' , 1),
+              'i2' => str_pad($field->attributes()->ind2 ? $field->attributes()->ind2->__toString() : '' , 1)
+           );
+           $subfieldQuery = $field->subfield;
+           foreach ($subfieldQuery as $subfield) {
+               $newField['s'][] = array($subfield->attributes()->code->__toString() => $subfield->__toString());
+           }
+           $tag = $field->attributes()->tag->__toString();
+           if (substr($tag, 0, 2) != '00') {
+               $this->fields[$tag][] = $newField;
+           }
+       }
     }
 
 }
